a378b34130945836588d593887160777535d7897
kamyar-nemati <kamyarnemati@gmail.com>
2017-08-25 12:03:52 +0800

    The game UI is now the default page.

--- tic-tac-toe/application/config/routes.php
+++ tic-tac-toe/application/config/routes.php
@@ -49,11 +49,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+//$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'TicTacToe'; //default page is now the game UI
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = TRUE;
 
-$route['game'] = 'TicTacToe';
+$route['game'] = 'TicTacToe'; //the URL to the game page
 
 /*
 | -------------------------------------------------------------------------
